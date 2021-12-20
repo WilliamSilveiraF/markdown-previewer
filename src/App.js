e8c8c255fd688388eda1ff0a1db28859ee489832
William Silveira <williamsilveiraf@hotmail.com>
2021-12-20 14:16:40 -0300

    bootstrap, marked, states, styles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import { marked } from "marked";
 
 function App() {
+  const [text, setText] = useState('# Bem-Vindo')
+  
+  const getMarkdown = () => {
+    const markdown = marked(text, { breaks: true })
+    return { __html: markdown }
+  }
+  
   return (
-    <section className='compiler'>
-      <textarea name='' id='editor'></textarea>
-      <article id='preview'>teste</article>
-    </section>
+    <main>
+      <section id='compiler' className="column mt-4">
+        
+        <div class="control form-group shadow-textarea col-10 m-auto">
+          <h1 className="mb-4 text-center">Converta seu Markdown:</h1>
+          <textarea 
+            class="form-control z-depth-1"
+            placeholder="Converta seu markdown:"
+            id="editor"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <article id='preview' className="control col-10 m-auto mt-4">
+          <div dangerouslySetInnerHTML={ getMarkdown() }></div>
+        </article>
+      </section>
+    </main>
   );
 }
 
